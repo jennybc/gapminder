9a83a2fd5a140026dc7035277ef525cb0908142b
jennybc <jenny@stat.ubc.ca>
2014-10-25 21:18:51 -0700

    ENCODING VICTORY IS MINE; split excel-extraction scripts by variable

--- data-raw/01_extract-from-excel-pop.r
+++ data-raw/01_extract-from-excel-pop.r
@@ -2,10 +2,11 @@ library(gdata)     # read.xls()
 library(dplyr)
 library(ggplot2)
 
-## Job 1: clean the population data
+## extract the population data
 
-## this is the Excel file I originally downloaded 2008-10-08
-pop_xls <- read.xls("xls/gapdata003.xls", verbose = TRUE)
+## this is the Excel file downloaded 2008-10-08 from gapminder.org
+pop_xls <- read.xls("xls/gapdata003.xls", verbose = TRUE,
+                    fileEncoding =  "ISO-8859-1")
 pop_xls %>% str
 ## 2014: 20455 obs. of  12 variables
 ## 2010 cleaning code comment: 22903 obs. of  10 variables <-- huh?
@@ -17,6 +18,10 @@ pop_xls %>% head
 pop_raw <- pop_xls %>%
   select(country = Area, year = Year, pop = Population)
 pop_raw %>% str
+# 'data.frame':  20455 obs. of  3 variables:
+# $ country: Factor w/ 253 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+# $ year   : int  1800 1820 1870 1913 1950 1951 1952 1953 1954 1955 ...
+# $ pop    : Factor w/ 19155 levels "0","1,000","1,000,000",..: 8705 8705 11243 13767 17502 17579 17652 17722 17793 17873 ...
 
 ## focus on the years where most of the data is
 summary(pop_raw$year)
@@ -40,12 +45,20 @@ year_max <- 2008
 pop_raw <- pop_raw %>%
   filter(year >= year_min & year <= year_max)
 str(pop_raw)                             # 14105 obs. of  3 variables:
+# 'data.frame':  14105 obs. of  3 variables:
+# $ country: Factor w/ 253 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+# $ year   : int  1950 1951 1952 1953 1954 1955 1956 1957 1958 1959 ...
+# $ pop    : Factor w/ 19155 levels "0","1,000","1,000,000",..: 17502 17579 17652 17722 17793 17873 18364 18453 18531 18602 ...
 
 ## get rid of the commas in pop (which is currently a factor!)
 pop_raw <- pop_raw %>%
   mutate(pop = levels(pop)[as.numeric(pop)]) %>%
   mutate(pop = as.numeric(gsub(",","", pop)))
 pop_raw %>% str # 14105 obs. of  3 variables
+# 'data.frame':  14105 obs. of  3 variables:
+# $ country: Factor w/ 253 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+# $ year   : int  1950 1951 1952 1953 1954 1955 1956 1957 1958 1959 ...
+# $ pop    : num  8150368 8284473 8425333 8573217 8728408 ...
 
 ## save for now
 write.table(pop_raw,
