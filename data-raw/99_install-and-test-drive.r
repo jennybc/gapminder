f9209e6686e176064235beec2cdcba89ef4276e7
jennybc <jenny@stat.ubc.ca>
2014-10-27 01:16:05 -0700

    test drive code and plot

--- data-raw/99_install-and-test-drive.r
+++ data-raw/99_install-and-test-drive.r
@@ -16,6 +16,22 @@ if (require("dplyr")) {
     summarise(lifeExp = median(lifeExp))
 }
 
-
+## README.md
 library(devtools)
 install_github("jennybc/gapminder")
+
+library(gapminder)
+
+aggregate(lifeExp ~ continent, gapminder, median)
+
+library(dplyr)
+gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarise(lifeExp = median(lifeExp))
+
+library(ggplot2)
+ggplot(gapminder, aes(x = continent, y = lifeExp)) +
+  geom_boxplot(outlier.colour = "hotpink") +
+  geom_jitter(position = position_jitter(width = 0.1, height = 0), alpha = 1/4)
+ggsave("../test-drive-stripplot.png")
