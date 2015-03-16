0c92f507dc9bdf6cc053447ce1242c02d23b7c34
jennybc <jenny@stat.ubc.ca>
2015-03-16 13:55:25 -0700

    beef up examples

--- R/gapminder.R
+++ R/gapminder.R
@@ -13,13 +13,36 @@
 #'   \item{gdpPercap}{GDP per capita}
 #'   }
 #' @source \url{http://www.gapminder.org/data/}
+#' @seealso \code{\link{country_colors}} for a nice color scheme for the countries
 #' @examples
+#' str(gapminder)
+#' head(gapminder)
+#' summary(gapminder)
+#' table(gapminder$continent)
+#' aggregate(lifeExp ~ continent, gapminder, median)
+#' plot(lifeExp ~ year, gapminder, subset = country == "Cambodia", type = "b")
+#' plot(lifeExp ~ gdpPercap, gapminder, subset = year == 2007, log = "x")
+#' 
 #' if (require("dplyr")) {
-#' gapminder
 #' gapminder %>%
 #'   filter(year == 2007) %>%
 #'   group_by(continent) %>%
 #'   summarise(lifeExp = median(lifeExp))
+#' 
+#' # how many unique countries does the data contain, by continent?
+#' gapminder %>%
+#'   group_by(continent) %>%
+#'   summarize(n_obs = n(), n_countries = n_distinct(country))
+#' 
+#' # by continent, which country experienced the sharpest 5-year drop in
+#' # life expectancy and what was the drop?
+#' gapminder %>%
+#'   group_by(continent, country) %>%
+#'   select(country, year, continent, lifeExp) %>%
+#'   mutate(le_delta = lifeExp - lag(lifeExp)) %>%
+#'   summarize(worst_le_delta = min(le_delta, na.rm = TRUE)) %>%
+#'   filter(min_rank(worst_le_delta) < 2) %>%
+#'   arrange(worst_le_delta)
 #' }
 #'
 "gapminder"
