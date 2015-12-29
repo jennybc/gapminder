a016d47940878ee60f8020fd4d100fa4afa8f5d5
jennybc <jenny@stat.ubc.ca>
2015-12-29 00:07:38 -0800

    re-merge data; continent has NA now, not ""

--- data-raw/04_merge-pop-lifeExp-gdpPercap.r
+++ data-raw/04_merge-pop-lifeExp-gdpPercap.r
@@ -1,48 +1,49 @@
 library(plyr)  ## revalue()
 library(dplyr)
 library(ggplot2)
+library(readr)
 
 ## bring in lightly cleaned datasets extracted from excel spreadsheets
 
-pop_dat <- read.delim("01_pop.tsv")
-pop_dat %>% str
-# 'data.frame':  14105 obs. of  3 variables:
+pop_dat <- read_tsv("01_pop.tsv") %>% 
+  mutate(country = factor(country))
+pop_dat %>% str()
+# Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	14105 obs. of  3 variables:
 # $ country: Factor w/ 253 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
 # $ year   : int  1950 1951 1952 1953 1954 1955 1956 1957 1958 1959 ...
-# $ pop    : num  8150368 8284473 8425333 8573217 8728408 ...
-## 2014: only difference with 2010 is pop is numeric now, was integer
-pop_dat %>% head
-pop_dat %>% tail
+# $ pop    : int  8150368 8284473 8425333 8573217 8728408 8891209 9061938 9240934 9428556 9624606 ...
+pop_dat %>% head()
+pop_dat %>% tail()
 
-le_dat <- read.delim("02_lifeExp.tsv")
-le_dat %>% str
-# 'data.frame':  3786 obs. of  4 variables:
+le_dat <- read_tsv("02_lifeExp.tsv") %>% 
+  mutate(country = factor(country),
+         continent = factor(continent))
+le_dat %>% str()
+# Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	3786 obs. of  4 variables:
 # $ country  : Factor w/ 198 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
-# $ continent: Factor w/ 7 levels "","Africa","Americas",..: 4 4 4 4 4 4 4 4 4 4 ...
+# $ continent: Factor w/ 6 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
 # $ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
 # $ lifeExp  : num  28.8 30.3 32 34 36.1 ...
-## 2014: only difference with 2010 is order of the variables
-le_dat %>% head
-le_dat %>% tail           
+le_dat %>% head()
+le_dat %>% tail()
 
-gdp_dat <- read.delim("03_gdpPercap.tsv")
-gdp_dat %>% str
-# 'data.frame':  10911 obs. of  3 variables:
+gdp_dat <- read_tsv("03_gdpPercap.tsv") %>% 
+  mutate(country = factor(country))
+gdp_dat %>% str()
+# Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	10911 obs. of  3 variables:
 # $ country  : Factor w/ 229 levels "Afghanistan",..: 1 2 3 4 6 9 10 12 13 14 ...
 # $ gdpPercap: num  757 1532 2429 4465 3363 ...
 # $ year     : int  1950 1950 1950 1950 1950 1950 1950 1950 1950 1950 ...
-## 2014: only difference with 2010 is order of the variables
-## 'data.frame':	10911 obs. of  3 variables:
-gdp_dat %>% head
-le_dat %>% tail
+gdp_dat %>% head()
+le_dat %>% tail()
 
 ## studying the overlap between countries in the different datasets
 country_levels <- function(df) levels(df$country)
 union_country <- country_levels(pop_dat) %>%
   union(country_levels(le_dat)) %>%
   union(country_levels(gdp_dat)) %>%
-  sort
-union_country %>% length # 271 (in 2010 and 2014)
+  sort()
+union_country %>% length() # 271
 union_country
 ## problems I see by eye
 # Bahamas  Bahamas, The
@@ -108,14 +109,14 @@ gdp_dat <- gdp_dat %>%
 union_country <- country_levels(pop_dat) %>%
   union(country_levels(le_dat)) %>%
   union(country_levels(gdp_dat)) %>%
-  sort
-union_country %>% length # 260 (in 2010 and 2014), down from 271
+  sort()
+union_country %>% length # 260, down from 271
 c_dat <- data_frame(country = union_country,
                     pop = country %in% levels(pop_dat$country),
                     le = country %in% levels(le_dat$country),
                     gdp = country %in% levels(gdp_dat$country),
                     total = pop + le + gdp)
-c_dat$total %>% table
+c_dat$total %>% table()
 ## BEFORE revalues    AFTER revalues    
 ##  1   2   3         1   2   3 
 ## 40  53 178        28  44 188 
@@ -129,8 +130,6 @@ pop_russia <- pop_dat %>%
   filter(country %in% c("Russia","USSR"))
 (ggplot(pop_russia, aes(x = year, y = pop, color = country)) +
    geom_line())
-xyplot(pop ~ year, type = "l", 
-            groups = country[drop = TRUE], auto.key = TRUE))
 ## huh?
 ## pop data present for USSR *and* Russia, 1950 - 2008
 ## USSR pop >> Russia pop, USSR presumably includes Russia??
@@ -145,20 +144,30 @@ gdp_dat %>%
 ## decision: keep countries found in all 3 datasets
 
 ## merge all three datasets!  then enforce countries to keep
+
+## 2015-12-29
+## dplyr bug means we can't use inner_join right now
+## https://github.com/hadley/dplyr/issues/1559
 gap_dat <- pop_dat %>%
-  inner_join(gdp_dat, by = c("country", "year")) %>%
-  inner_join(le_dat, by = c("country", "year")) %>%
-  droplevels %>%
+#  inner_join(gdp_dat, by = c("country", "year")) %>%
+#  inner_join(le_dat, by = c("country", "year")) %>%
+  merge(gdp_dat, by = c("country", "year")) %>% 
+  merge(le_dat, by = c("country", "year")) %>%
+  droplevels() %>%
   arrange(country, year)
-gap_dat %>% str
-# 'data.frame':  3312 obs. of  6 variables:
+
+gap_dat %>% str()
+# 'data.frame':	3312 obs. of  6 variables:
 # $ country  : Factor w/ 187 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
 # $ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
-# $ pop      : num  8425333 9240934 10267083 11537966 13079460 ...
+# $ pop      : int  8425333 9240934 10267083 11537966 13079460 14880372 12881816 13867957 16317921 22227415 ...
 # $ gdpPercap: num  779 821 853 836 740 ...
-# $ continent: Factor w/ 7 levels "","Africa","Americas",..: 4 4 4 4 4 4 4 4 4 4 ...
+# $ continent: Factor w/ 6 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
 # $ lifeExp  : num  28.8 30.3 32 34 36.1 ...
-## 2014: agrees with merged result in 2010, except for
+## 2015: agrees with merged result in 2014, except for
+##   * pop is int now (as it should be and as it was in 2010)
+##   * continent used to have 7 levels because we had "" instead of NA (I think)
+## 2014: agreed with merged result in 2010, except for
 ##   * variable order
 ##   * pop is numeric now, was integer then
 ##   * at this point in 2010 cleaning, I had an unused level for the country
@@ -168,6 +177,4 @@ my_vars <- c('country', 'continent', 'year',
              'lifeExp', 'pop', 'gdpPercap')
 gap_dat <- gap_dat[my_vars]
 
-write.table(gap_dat,
-            "04_gap-merged.tsv",
-            quote = FALSE, sep = "\t", row.names = FALSE)
+write_tsv(gap_dat, "04_gap-merged.tsv")
--- data-raw/06_fill-and-fix-continent.r
+++ data-raw/06_fill-and-fix-continent.r
@@ -1,36 +1,37 @@
 library(plyr)
 library(dplyr)
 library(ggplot2)
+library(readr)
 
-gap_dat <- read.delim("04_gap-merged.tsv")
+gap_dat <- read_tsv("04_gap-merged.tsv") %>% 
+  mutate(country = factor(country),
+         continent = factor(continent))
 gap_dat %>% str
-# 'data.frame':  3312 obs. of  6 variables:
+# Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	3312 obs. of  6 variables:
 # $ country  : Factor w/ 187 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+# $ continent: Factor w/ 6 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
 # $ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
-# $ pop      : num  8425333 9240934 10267083 11537966 13079460 ...
-# $ gdpPercap: num  779 821 853 836 740 ...
-# $ continent: Factor w/ 7 levels "","Africa","Americas",..: 4 4 4 4 4 4 4 4 4 4 ...
 # $ lifeExp  : num  28.8 30.3 32 34 36.1 ...
-
-gap_dat$continent %>% summary
-##           Africa Americas     Asia   Europe      FSU  Oceania 
-##     301      613      343      557     1302      122       74
-
+# $ pop      : int  8425333 9240934 10267083 11537966 13079460 14880372 12881816 13867957 16317921 22227415 ...
+# $ gdpPercap: num  779 821 853 836 740 ...
+gap_dat$continent %>% summary()
+# Africa Americas     Asia   Europe      FSU  Oceania     NA's 
+#    613      343      557     1302      122       74      301 
 ## 301 rows have no continent data.  That is a problem.
 
 ## I've never heard of the continent of FSU.
 tmp <- gap_dat %>%
   filter(continent == "FSU") %>%
-  droplevels
-tmp$country %>% levels
+  droplevels()
+tmp$country %>% levels()
 ## [1] Belarus    Kazakhstan Latvia     Lithuania  Russia     Ukraine
 ## FSU = Former Soviet Union (?)
 
 ## which countries do not have continent data?
 tmp <- gap_dat %>%
-  filter(continent == "") %>%
-  droplevels
-tmp$country %>% levels
+  filter(is.na(continent)) %>%
+  droplevels()
+tmp$country %>% levels()
 # [1] "Armenia"               "Aruba"                 "Australia"            
 # [4] "Bahamas"               "Barbados"              "Belize"               
 # [7] "Canada"                "French Guiana"         "French Polynesia"     
@@ -75,23 +76,28 @@ cont_dat <- c('country, continent',
 cont_dat <- read.csv(text = cont_dat, strip.white = TRUE)
 
 gap_dat <- gap_dat %>%
-  left_join(cont_dat, by = "country") %>%
+  ## 2015-12-29
+  ## dplyr bug means we can't use inner_join right now
+  ## https://github.com/hadley/dplyr/issues/1559
+  #left_join(cont_dat, by = "country") %>%
+  merge(cont_dat, by = "country", all = TRUE) %>% 
+  tbl_df() %>%
   mutate(continent = factor(ifelse(is.na(continent.y),
                                    as.character(continent.x),
                                    as.character(continent.y))),
          continent.x = NULL,
          continent.y = NULL) %>%
   arrange(country, year)
-gap_dat %>% str
-# 'data.frame':  3312 obs. of  6 variables:
+gap_dat %>% str()
+# Classes ‘tbl_df’, ‘tbl’ and 'data.frame':	3312 obs. of  6 variables:
 # $ country  : Factor w/ 187 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
 # $ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
-# $ pop      : num  8425333 9240934 10267083 11537966 13079460 ...
-# $ gdpPercap: num  779 821 853 836 740 ...
 # $ lifeExp  : num  28.8 30.3 32 34 36.1 ...
+# $ pop      : int  8425333 9240934 10267083 11537966 13079460 14880372 12881816 13867957 16317921 22227415 ...
+# $ gdpPercap: num  779 821 853 836 740 ...
 # $ continent: Factor w/ 6 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
 
-gap_dat$continent %>% summary
+gap_dat$continent %>% summary()
 # Africa Americas     Asia   Europe      FSU  Oceania 
 #    637      470      577     1302      139      187 
 
@@ -99,6 +105,4 @@ my_vars <- c('country', 'continent', 'year',
              'lifeExp', 'pop', 'gdpPercap')
 gap_dat <- gap_dat[my_vars]
 
-write.table(gap_dat,
-            "06_gap-merged-with-continent.tsv",
-            quote = FALSE, sep = "\t", row.names = FALSE)
+write_tsv(gap_dat, "06_gap-merged-with-continent.tsv")
