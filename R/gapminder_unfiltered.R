c5e147c6afb15ae51ead9371979884a07a7d1436
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-03-08 08:38:58 -0800

    Update gapminder_unfiltered help

--- R/gapminder_unfiltered.R
+++ R/gapminder_unfiltered.R
@@ -2,5 +2,5 @@
 #' 
 #' The supplemental data frame `gapminder_unfiltered` was not filtered on 
 #' `year` or for complete data and has 3313 rows. Everything else is as
-#' documented in [gapminder()].
+#' documented in [`gapminder`].
 "gapminder_unfiltered"
